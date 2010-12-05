89f22815a131bcef04c4bf7436bc05768d2156bc
JoshAshby <joshuaashby@joshashby.com>
2010-12-05 15:08:43 -0700

    and again more comments, getting ready to test it if i can get my atmega to work today (was throwing a fit last night)

--- main.c
+++ main.c
@@ -20,12 +20,16 @@ int main(void) { //Main loop, runs once but can have an infinit loop in it
     uart_start();
     twi_start();
 
-    while(1) { //infinit loop that doesn't stop running.
-        temp = (PIND & 0x0c);
-        if (temp == 1){
+    while(1) { //infinit loop that doesn't stop running. (always true since 1 is always 1
+
+        //sample button text, eventually this will become the meet and bones of
+        //digital.c running as an interrupt code which also debounces the button
+        //and letting the main code know that a button has been pressed, and which one
+        temp = (PIND & 0x0c); //temp is the 3rd and 4th pins of port D
+        if (temp == 1){ //If either are pulled high then turn pin 1 on port B on
             out('B',1,1);
         }
-        if (temp == 0){
+        if (temp == 0){ //if either are off, turn pin1 port B off
             out('B',1,0);
         }
     };
